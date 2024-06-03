161e8ec89b9764049f0ba10c79356f43aeaffea7
feraistanti <fistanti551@gmail.com>
2024-06-03 15:24:26 +0700

    memasukan int main,menambah try,catch

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try {
+        cout << "selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0,5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengecualiann akan di eksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini akan diesekusi*/
+        cout << "default pengecualian diexsekusi" << endl;
+    }
+    return 0;
+}
