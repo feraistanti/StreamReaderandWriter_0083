f075bd3f16c302034154b569297bd90fc7f6de51
feraistanti <fistanti551@gmail.com>
2024-06-03 15:22:32 +0700

    memasukan int main

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -4,3 +4,20 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda
+    try {
+        array<int, 3> data = { 8, 4, 2};
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+    
+}
